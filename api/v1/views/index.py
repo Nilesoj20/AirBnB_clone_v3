d626e7bf2c2d3dc662374b55217b7d1b65150bbe
Nilesoj <118998391+Nilesoj20@users.noreply.github.com>
2023-08-26 11:46:47 -0500

    fix:  Update index to display all objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """ index method for API development """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'], strict_slaches=False)
@@ -9,3 +16,16 @@ def status():
     """ method to check the API status  """
     return jsonify({"status": "OK"})
 
+
+@app_views.route("/api/v1/stats", methods=['GET'], strict_slaches=False)
+def total():
+    """ retrieves the number of each object by type """
+    form = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+    }
+    return jsonify(form)
