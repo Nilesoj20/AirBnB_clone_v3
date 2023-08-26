8b38e3a91270413e5047f300f54cae80e1c2e696
Nilesoj <118998391+Nilesoj20@users.noreply.github.com>
2023-08-26 11:47:57 -0500

    fix: adding missing documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" installation of Blueprint """
+""" API Flash Manager """
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
+""" Imports requested in the task """
 from api.v1.views.index import *
