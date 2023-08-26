bf214d2300cbb95bfe3603bb397d71353b29994e
Nilesoj <118998391+Nilesoj20@users.noreply.github.com>
2023-08-26 16:42:32 -0500

    fix: Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-""" index method for API development """
+"""Returns JSON status"""
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
 from models.user import User
+from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slaches=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
-    """ method to check the API status  """
+    """Returns the status of our API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/api/v1/stats", methods=['GET'], strict_slaches=False)
-def total():
-    """ retrieves the number of each object by type """
-    form = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User)
-    }
-    return jsonify(form)
+@app_views.route('/stats', methods=["GET"], strict_slashes=False)
+def count_objects():
+    """Counts the number of objects by type"""
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_obj = {}
+    for i in range(len(classes)):
+        num_obj[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_obj)
