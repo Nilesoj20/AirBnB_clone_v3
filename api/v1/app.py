dbf07c7a14e700e9fac064b3a4b541d776f194fb
Nilesoj <118998391+Nilesoj20@users.noreply.github.com>
2023-08-26 16:41:22 -0500

    fix: change the way I use imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""API development with flask """
-from flask import Flask, make_response, jsonify
+"""Creating the API and returning the status"""
+
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close():
-    """methods for handling @app.teardown_appcontext"""
+def teardown_app(exception):
+    """Tears down by calling storage.close"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_fount(error):
-    """ Json with error 404 not found """
-    return make_response(jsonify({'error': "Not found"}), 404)
+def not_found(exception):
+    """404 error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    """the main function main"""
-    host = environ.get("HBNB_API_HOST")
-    port = environ.get("HBNB_API_PORT")
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = 5000
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST",
+                        default="0.0.0.0"),
+            port=getenv("HBNB_API_PORT",
+                        default="5000"),
+            threaded=True)
